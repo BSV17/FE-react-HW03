2bc455571347ac0555664894c5ee0023674448aa
BSV17 <seregars17@gmail.com>
2022-06-28 23:59:11 +0300

    Done react homework 4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css"
+import PostForm from "./components/PostForm/PostForm";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            posts: []
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(response => response.json())
+            .then(data => this.setState({posts: data}))
+    }
+
+    handleDeletePost(id) {
+        let updatedPostsList = this.state.posts.filter((post) => post.id !== id);
+        this.setState({posts: updatedPostsList})
+    }
+
+    handleAdd = (event, title, textContent, id) => {
+        event.preventDefault();
+
+        fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            body: JSON.stringify({
+                title: title,
+                body: textContent,
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        })
+            .then((response) => response.json())
+            .then((json) => {
+                let newData = this.state.posts
+                newData.unshift(json);
+                this.setState({posts: newData})
+            });
+    };
+
+    handleEdit = (event, title, textContent, id) => {
+        event.preventDefault();
+
+        fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+            method: 'PUT',
+            body: JSON.stringify({
+                title: title,
+                body: textContent,
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        })
+            .then((response) => response.json())
+            .then((json) => {
+                let updatedPosts =  this.state.posts.map(item => item.id === id ? json : item);
+
+                this.setState({posts: updatedPosts})
+            });
+    };
+
+    render() {
+        return (
+            <>
+                <PostForm handleSubmit={this.handleAdd}/>
+                <ul className="list">
+                    {this.state.posts.map((item) =>
+                        <li key={item.id} className="list-item">
+                            <h2>{item.id}. {item.title}</h2>
+                            <p>{item.body}</p>
+                            <button className="list-item-button"
+                                    onClick={() => this.handleDeletePost(item.id)}>Delete
+                            </button>
+                            <PostForm handleSubmit={this.handleEdit} item={item}/>
+                        </li>)}
+                </ul>
+            </>
+        )
+    }
 }
 
 export default App;
---
+++ src/components/PostForm/PostForm.js
@@ -0,0 +1,82 @@
+import React from "react";
+import './Postform.css';
+
+class PostForm extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            title: "",
+            textContent: "",
+            isValid: false,
+            errors: {
+                title: false,
+                textContent: false
+            }
+        }
+    }
+
+    componentDidMount() {
+        if (this.props.item) {
+            this.setState({title: this.props.item.title});
+            this.setState({textContent: this.props.item.body});
+        }
+    }
+
+    validateData = (e) => {
+        let regex = /^(?=.*[a-z])(?=.*[A-Z])[a-zA-Z]{2,7}$/;
+        if (regex.test(e.target.value) || e.target.value === "") {
+            this.setState({errors: {...this.state.errors, [e.target.name]: false}});
+            return true;
+        } else {
+            this.setState({errors: {...this.state.errors, [e.target.name]: true}});
+        }
+        return false;
+    }
+
+
+    handleChange = (event) => {
+        this.setState({[event.target.name]: event.target.value});
+        if (this.validateData(event) && this.state.title !== "" && this.state.textContent !== "") {
+            this.setState({isValid: true});
+        } else {
+            this.setState({isValid: false});
+        }
+    }
+
+    handlePost = (event) => {
+        event.preventDefault();
+        this.props.handleSubmit(event, this.state.title, this.state.textContent, this.props.item?.id);
+        this.setState({title: ""});
+        this.setState({textContent: ""});
+    }
+
+
+    render() {
+        return (
+            <form className="post-form" onSubmit={(event) => this.handlePost(event)}>
+                <input
+                    type="text"
+                    name="title"
+                    placeholder="Enter post title..."
+                    value={this.state.title}
+                    onChange={this.handleChange}
+                    className={this.state.errors.title ? "error" : ""}
+                />
+                <textarea
+                    type="text"
+                    name="textContent"
+                    placeholder="Enter post content..."
+                    value={this.state.textContent}
+                    onChange={this.handleChange}
+                    className={this.state.errors.textContent ? "error" : ""}
+                />
+                <button type="submit" className="submit-button"
+                        disabled={!this.state.isValid}>
+                    {this.props.item ? "Edit" : "Add"}
+                </button>
+            </form>
+        )
+    }
+}
+
+export default PostForm;
